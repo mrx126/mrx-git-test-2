0dae4852e0cc68b2ab7a2b67d2ac1451492cfb4d
mrx126 <mirzaabdillahfakih@gmail.com>
2022-10-22 17:53:38 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore cum aliquam, quidem earum rerum laborum repellendus excepturi impedit accusamus consequatur fugit voluptate eos quas autem architecto illo incidunt. Eveniet, asperiores!</p>
 </body>
 </html>
